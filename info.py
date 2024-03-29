c8dd27063faa2878f931922659afe1bd7f2df873
ALSOONYT <162725524+ALSOONYT@users.noreply.github.com>
2024-03-29 11:24:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -87,18 +87,18 @@ SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "seas
 
 
 
-# Online Stream and Download
+Online Stream and Download
 NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = environ.get('alas-now-abhishek')
+    APP_NAME = environ.get('APP_NAME')
 else:
-    ON_HEROKU = True
+    ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = ("https://alas-now-abhishek-f8e6f1b10e16.herokuapp.com") if ON_HEROKU or NO_PORT else \
-    ("https://alas-now-abhishek-f8e6f1b10e16.herokuapp.com")
+URL = "https://alas-now-abhishek-f8e6f1b10e16.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://alas-now-abhishek-f8e6f1b10e16.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -107,15 +107,15 @@ name = str(environ.get('name', 'LazyPrincess'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = str(getenv('alas-now-abhishek'))
+    APP_NAME = str(getenv('APP_NAME'))
 
 else:
-    ON_HEROKU = True
+    ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://alas-now-abhishek-f8e6f1b10e16.herokuapp.com
+    URL = "https://alas-now-abhishek-f8e6f1b10e16.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://alas-now-abhishek-f8e6f1b10e16.herokuapp.com
+    URL = "https://alas-now-abhishek-f8e6f1b10e16.herokuapp.com/".format(FQDN)
 
 
 
